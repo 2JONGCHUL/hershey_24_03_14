a55c8fd4b441d597e9713478f0a9d7fefefb2f3f
2JONGCHUL <100770186+2JONGCHUL@users.noreply.github.com>
2024-03-14 20:13:20 +0900

    Add files via upload

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,5 +1,6 @@
 console.clear();
 
+// nav bar hover
 function nav__hover() {
   $(".top-bar__nav > ul > li").mouseenter(function () {
     $(".top-bar").css("backgroundColor", "white");
@@ -13,6 +14,37 @@ function nav__remv() {
     $(".top-bar__nav > ul > li > a").removeClass("hover");
   });
 }
-
 nav__hover();
 nav__remv();
+
+ScrollTrigger;
+ScrollTrigger.create({
+  trigger: ".hersheys-menu__banner",
+  start: "top 40%",
+  end: "bottom 50%",
+  // endTrigger: "",
+  markers: true,
+  // toggleClass: "active",
+  onEnter: function () {
+    // 위에서 아래로 내려오다가 트리거 엘리먼트의 정수리를 발견했을 때
+    $(".hersheys-menu__banner").addClass("on");
+  },
+  onLeave: function () {
+    $(".choco1").addClass("on");
+  },
+});
+
+function SwiperSlide1__init() {
+  const swiper = new Swiper(".swiper-slide-box-1 .swiper", {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    speed: 1000,
+    autoplay: {
+      delay: 2000,
+    },
+  });
+}
+
+SwiperSlide1__init();
